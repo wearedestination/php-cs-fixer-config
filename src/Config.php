7ed3d6035af57c157a42bec2e58ab31ef66ed029
andyexeter <palmer.andy@gmail.com>
2021-06-24 09:50:04 +0100

    Update rules for PHP 7.4 and 8

--- src/Config.php
+++ src/Config.php
@@ -11,17 +11,28 @@ final class Config extends \PhpCsFixer\Config
         $this->setRules([
             '@Symfony'                      => true,
             '@Symfony:risky'                => true,
-            'array_syntax'                  => ['syntax' => 'short'],
+            'declare_strict_types'          => true,
+            // Prevent Psalm inline annotations being stripped. https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4446
+            'phpdoc_to_comment'             => false,
             'concat_space'                  => ['spacing' => 'one'],
             'fopen_flags'                   => false,
             'ordered_imports'               => true,
             'protected_to_private'          => false,
-            'binary_operator_spaces'        => ['align_double_arrow' => true, 'align_equals' => true],
+            'binary_operator_spaces'        => [
+                'operators' => [
+                    '=>' => 'align',
+                    '='  => 'align',
+                    // Prevent spaces being added in PHP8 union types
+                    '|'  => null,
+                ],
+            ],
+            // Bad yoda style is
             'yoda_style'                    => false,
-            'phpdoc_inline_tag'             => false,
             'phpdoc_annotation_without_dot' => false,
             'cast_spaces'                   => ['space' => 'none'],
-            'no_superfluous_phpdoc_tags'    => true,
+            'no_superfluous_phpdoc_tags'    => [
+                'allow_mixed' => true,
+            ],
             'echo_tag_syntax'               => false,
         ]);
 
