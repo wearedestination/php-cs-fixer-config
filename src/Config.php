23374b6bf57666288cd909fd846e902fa56c54d5
andyexeter <palmer.andy@gmail.com>
2023-09-28 10:36:47 +0100

    Add trailing comma in multi line argument/parameter lists and match statements

--- src/Config.php
+++ src/Config.php
@@ -11,16 +11,16 @@ final class Config extends \PhpCsFixer\Config
         parent::__construct('Destination');
 
         $this->setRules([
-            '@Symfony'                      => true,
-            '@Symfony:risky'                => true,
-            'declare_strict_types'          => true,
+            '@Symfony'                                         => true,
+            '@Symfony:risky'                                   => true,
+            'declare_strict_types'                             => true,
             // Prevent Psalm inline annotations being stripped. https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4446
-            'phpdoc_to_comment'             => false,
-            'concat_space'                  => ['spacing' => 'one'],
-            'fopen_flags'                   => false,
-            'ordered_imports'               => true,
-            'protected_to_private'          => false,
-            'binary_operator_spaces'        => [
+            'phpdoc_to_comment'                                => false,
+            'concat_space'                                     => ['spacing' => 'one'],
+            'fopen_flags'                                      => false,
+            'ordered_imports'                                  => true,
+            'protected_to_private'                             => false,
+            'binary_operator_spaces'                           => [
                 'operators' => [
                     '=>' => 'align',
                     '='  => 'align',
@@ -44,6 +44,10 @@ final class Config extends \PhpCsFixer\Config
             'single_line_throw'                                => false,
             'use_arrow_functions'                              => true,
             'nullable_type_declaration_for_default_null_value' => true,
+            'trailing_comma_in_multiline'                      => [
+                'after_heredoc' => false,
+                'elements' => ['arguments', 'arrays', 'match', 'parameters'],
+            ],
         ]);
 
         $this->setRiskyAllowed(true);
